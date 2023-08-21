309d94ad7353ab9e2f8d094d4b935e43bb62dc38
Elijah Spina, PhD <ejspina@gmail.com>
2023-08-20 23:18:50 -0700

    Update widgets.js

--- src/data/widgets.js
+++ src/data/widgets.js
@@ -19,7 +19,7 @@ const TestnetWidgets = {
 
 const MainnetWidgets = {
   image: "mob.near/widget/Image",
-  default: "flowscience.near/widget/CGD.Page",
+  default: "opencann.near/widget/dashboard",
   viewSource: "mob.near/widget/WidgetSource",
   widgetMetadataEditor: "mob.near/widget/WidgetMetadataEditor",
   widgetMetadata: "mob.near/widget/WidgetMetadata",
