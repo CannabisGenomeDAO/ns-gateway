95813739bb0fe0c5b67ae4be53286abc3891b740
Elijah Spina, PhD <ejspina@gmail.com>
2023-09-29 12:53:20 -0700

    Update and rename NearSocialLogo.js to OpenCannLogo.js

---
+++ src/components/icons/OpenCannLogo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function OpenCannLogo() {
+  return (
+    <img
+      src="https://i.ibb.co/WvjTXM5/Open-Cann-Icon-2023.png"
+      alt="OpenCann"
+      width="40"
+      height="40"
+      className="logotype"
+    />
+  );
+}
