5a4f3894f2c4bbcb4a309517b021fb5d22c1468a
Elijah Spina, PhD <ejspina@gmail.com>
2023-09-29 12:05:06 -0700

    Update Logotype.js

--- src/components/navigation/Logotype.js
+++ src/components/navigation/Logotype.js
@@ -5,8 +5,8 @@ export function Logotype() {
     <img
       src="https://i.ibb.co/LtXM82q/Copy-of-Open-Cann.png"
       alt="OpenCann"
-      width="150"
-      height="50"
+      width="180"
+      height="60"
       className="logotype"
     />
   );
